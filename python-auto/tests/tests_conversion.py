6f7d16ef9eddb3c5b62a8a7ae5899142d2cb6624
Michael Mauderer <mail@michaelmauderer.com>
2016-10-20 20:22:45 +0100

    Setting up test framework to explore API design.

---
+++ python-auto/tests/tests_conversion.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Defines initial unit tests to explore API design and specify functionality.
+"""
+
+from __future__ import division, unicode_literals
+
+import numpy as np
+import unittest
+from itertools import permutations
+
+__author__ = 'Colour Developers'
+__copyright__ = 'Copyright (C) 2013-2016 - Colour Developers'
+__license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
+__maintainer__ = 'Colour Developers'
+__email__ = 'colour-science@googlegroups.com'
+__status__ = 'Production'
+
+__all__ = ['test_conversion_spectral_to_YCbCR',
+           'test_conversion_YCbCR_to_CIECAM02']
+
+
+def test_conversion_spectral_to_YCbCR():
+    pass
+
+
+def test_conversion_YCbCR_to_CIECAM02():
+    pass
