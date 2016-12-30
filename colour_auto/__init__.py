e04a5ee04223d220f78d7e74d9220d46a0d0123c
Michael Mauderer <mail@michaelmauderer.com>
2016-12-30 14:46:29 +0000

    Update documentation. Add conversion function to graph edges.

--- colour_auto/__init__.py
+++ colour_auto/__init__.py
@@ -7,15 +7,15 @@ Colour Auto
 
 from __future__ import absolute_import
 
-from .convert import *  # noqa
 from . import convert
+from .convert import *  # noqa
 
 __author__ = 'Colour Developers'
 __copyright__ = 'Copyright (C) 2013-2016 - Colour Developers'
 __license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-science@googlegroups.com'
-__status__ = 'Production'
+__status__ = 'Developement'
 
 __all__ = []
 __all__ += convert.__all__
--- colour_auto/common.py
+++ colour_auto/common.py
@@ -10,21 +10,20 @@ from __future__ import absolute_import
 import networkx as nx
 
 from colour.metadata.common import Metadata
-import colour.metadata.models
 
 __author__ = 'Colour Developers'
 __copyright__ = 'Copyright (C) 2013-2016 - Colour Developers'
 __license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-science@googlegroups.com'
-__status__ = 'Production'
+__status__ = 'Developement'
 
 __application_name__ = 'Colour-auto'
 
 
-def get_all_model_functions():
+def get_all_colour_model_functions():
     """
-    Return all models from the family 'Colour Model'.
+    Yields all model functions from the family 'Colour Model Functions'.
     """
     metadata_instances = Metadata._INSTANCES
     for metadata_instance_key, metadata_instance in \
@@ -34,10 +33,20 @@ def get_all_model_functions():
 
 
 def build_conversion_graph():
+    """
+    Return conversion graph that contains all colour model functions as edges.
+
+    Returns
+    -------
+    networkx.MultiDiGraph
+        Directed Multigraph of all colour model conversion fucntions.
+    """
     conversion_graph = nx.MultiDiGraph()
-    for model_function in get_all_model_functions():
+    for model_function in get_all_colour_model_functions():
         conversion_graph.add_edge(model_function.input_entity,
-                                  model_function.output_entity)
+                                  model_function.output_entity,
+                                  attr_dict={
+                                      'conversion_function': model_function})
     return conversion_graph
 
 
